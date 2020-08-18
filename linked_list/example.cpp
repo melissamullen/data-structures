8f5911b56de3fbb72ca2c95064ca4aa4a277a3f5
Melissa Mullen <44929819+melissamullen@users.noreply.github.com>
2020-08-18 11:40:27 -0400

    Update example.cpp

--- linked_list/example.cpp
+++ linked_list/example.cpp
@@ -4,10 +4,14 @@
 #include "linked_list.h"
 
 int main() {
+  // Create an integer implementation of linked list 
   LinkedList<int> list;
 
+  // Push values to linked list
   list.Push(1);
   list.Push(2);
   list.Push(5);
+  
+  // Print linked list
   list.PrintLinkedList();
 }
