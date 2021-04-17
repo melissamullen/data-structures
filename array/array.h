1d6338362497c7eab5f0223973dc69b79f0f6c83
Melissa Mullen <melissa.ann.mullen@gmail.com>
2021-04-17 10:51:21 -0400

    changed example

--- array/array.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARRAY_H
-#define ARRAY_H
-
-void PrintArray(int array[], int length);
-
-#endif
--- array/example.c
+++ array/example.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "array.h"
 
 int main() {
 
   // Define array of integers
   int array[] = {1,2,3,4,5};
 
-  // Print array
-  PrintArray(array, 5);
+  // Access value via index in O(1) time
+  int value = array[2];
+  printf("Access value at index 2: %d\n", value);
+
+  // Array name is a pointer to the address of the first element in the array
+  int *ptr = array;
+  int first_element = *ptr;
+  printf("First element is: %d\n", first_element);
 
   return 0;
 }
