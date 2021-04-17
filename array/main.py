09abcc4e1328adb035af720a4f43f5bc46df2379
Melissa Mullen <melissa.ann.mullen@gmail.com>
2021-04-16 23:20:48 -0400

    adding array

---
+++ array/main.py
@@ -0,0 +1,21 @@
+from array import *
+import pdb
+
+# Create array object
+array = Array()
+
+# Push values to array
+array.push(1)
+array.push(2)
+array.push(3)
+array.push(4)
+array.push(5)
+
+# Pop value from array
+n = array.pop()
+
+# Print popped value
+print(n)
+
+# Print rest of array
+array.print_array()
