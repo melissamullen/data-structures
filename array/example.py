7a5a40d16fbbf610aac555ddcabb01925ac25ae3
Melissa Mullen <melissa.ann.mullen@gmail.com>
2021-04-17 11:12:20 -0400

    changed example

---
+++ array/example.py
@@ -0,0 +1,11 @@
+import array
+import numpy as np
+
+# Create an array using the array module
+arr = array.array('i', [1, 2, 3, 4, 5])
+
+# Create an array using the numpy module
+np_arr = np.array([1, 2, 3, 4, 5, 6])
+
+# Create a Python list
+list = [1, 2, 3, 4, 5, 6, 7]
