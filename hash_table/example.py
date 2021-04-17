2a4b72a225388ec7e535fb9ad36608bf9cb26ae3
Melissa Mullen <melissa.ann.mullen@gmail.com>
2021-04-17 12:48:57 -0400

    small change

--- hash_table/example.py
+++ hash_table/example.py
@@ -1,9 +1,14 @@
 from hash_table import *
 
+# Create a hash table
 hash_table = HashTable()
+
+# Inser key/value pairs
 hash_table.insert(25, "Melissa")
 hash_table.insert(35, "Emmy")
 hash_table.insert(1000, "Joe")
+
+# Display the hash table
 hash_table.display()
 
 # Search for a key/value pair
